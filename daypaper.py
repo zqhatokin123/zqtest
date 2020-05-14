e19048e57d74f07ba64f7e41f25bac4ff736384e
zqhatokin123 <65356645+zqhatokin123@users.noreply.github.com>
2020-05-15 01:25:41 +0800

    Update daypaper.py

--- daypaper.py
+++ daypaper.py
@@ -5,11 +5,11 @@ import time
 url = 'https://oapi.dingtalk.com/robot/send?access_token=da97b17395414ceaa8e22dff61b7813b7badc433032b2ae5ad51c564b5eee361'
 
 obj = dict(msgtype="text", text=dict(content='实战日报: ' + time.strftime('%Y-%m-%d', time.localtime(time.time()))+
-                                             '\n各位还在局点的实战同学请将每日研判案件录入到“实战运营平台”（当日研判案件均需录入）;'
-                                             '\n平台链接如下：'
-                                             '\nhttp://vcs.gts.terminus.io/project/user-arrangement'
-                                             '\n实战日报如下:'
-                                             '\nhttp://vcs.gts.terminus.io/dashboard/quickbi-report'
+                                             '\n各位还在局点的实战同学请将每日研判案件录入到“实战运营平台”（当日研判案件均需录入）;'+
+                                             '\n平台链接如下：'+
+                                             '\nhttp://vcs.gts.terminus.io/project/user-arrangement'+
+                                             '\n实战日报如下:'+
+                                             '\nhttp://vcs.gts.terminus.io/dashboard/quickbi-report'+
                                              '\n辛苦各位同学啦！'))
 requests.post(url,
     headers={'Content-Type': 'application/json'},
