aeb2aa8a39e7e98dd494c4dad8d14a0bcdb4cc62
zqhatokin123 <65356645+zqhatokin123@users.noreply.github.com>
2020-05-15 11:04:58 +0800

    Update daypaper.py

--- daypaper.py
+++ daypaper.py
@@ -4,7 +4,8 @@ import time
 
 url = 'https://oapi.dingtalk.com/robot/send?access_token=53a5a9db50790c5c9468170930262d6b79c8669583b996da40dae5df2b4400e3'
 
-obj = dict(msgtype="text", text=dict(content='实战日报: ' + time.strftime('%Y-%m-%d', time.localtime(time.time()))+
+obj = dict(msgtype="text", text=dict(content='@所有人'+
+                                             '\n实战日报: ' + time.strftime('%Y-%m-%d', time.localtime(time.time()))+
                                              '\n各位还在局点的实战同学请将每日研判案件录入到“实战运营平台”（当日研判案件均需录入）;'+
                                              '\n平台链接如下：'+
                                              '\nhttp://vcs.gts.terminus.io/project/user-arrangement'+
